18c954df0e4c183dcb0b4034e156d61b01055912
Logan Barsell <loganjbars@gmail.com>
2024-07-07 14:15:09 -0700

    Create posts and comments w/ moderation + event bus + query

---
+++ event-bus/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+process.on("uncaughtException", function (err) {
+  console.log(err);
+});
+
+const events = [];
+
+app.post('/events', (req, res) => {
+  const event = req.body;
+  events.push(event);
+  // posts service
+  axios.post('http://localhost:4000/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  // comments service
+  axios.post('http://localhost:4001/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  // query service
+  axios.post('http://localhost:4002/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  // moderation service
+  axios.post('http://localhost:4003/events', event).catch((err) => {
+    console.log(err.message);
+  });
+  res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
+});
+
+app.listen(4005, () => {
+  console.log("Listening on 4005");
+});
